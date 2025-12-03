b37ee5154a2ef906476e21275dd6b1bdf3b339cf
strajk- <7372902+strajk-@users.noreply.github.com>
2025-12-03 14:24:52 +0100

    Update assembly title and product name

--- CustomShellManager/Properties/AssemblyInfo.cs
+++ CustomShellManager/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("ContextMenuRegistration")]
+[assembly: AssemblyTitle("CustomShellManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ContextMenuRegistration")]
+[assembly: AssemblyProduct("CustomShellManager")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
